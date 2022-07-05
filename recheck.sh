4a6941ea083cfc732aa95178f916347cfc7a7b6a
Peerapat Sattapornnara <93782805+Phepattt2@users.noreply.github.com>
2022-07-05 15:36:21 +0700

    Add files via upload

---
+++ recheck.sh
@@ -0,0 +1,9 @@
+# change path to your moxa directory  
+cd /home/peerapats/Desktop/moxa/moxa-uport-1100-series-linux-kernel-5.x-driver-v5.1/mxu11x0
+
+sudo make remove
+sudo ./mxinstall
+
+sudo modprobe mxu11x0
+
+
